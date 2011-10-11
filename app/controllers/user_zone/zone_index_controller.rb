5c84a0b260dc4356518f31bbb066f7d6d67f0c3a
Rei Dai <daigong@qq.com>
2011-10-11 17:10:01 +0800

    修改时区设置、跳转bug没有传递参数、若干注释

--- app/controllers/user_zone/zone_index_controller.rb
+++ app/controllers/user_zone/zone_index_controller.rb
@@ -8,6 +8,8 @@ class UserZone::ZoneIndexController < ApplicationController
     @activities = @admin_person.now_can_join_activities
     if @activities.size == 1
       #TODO 如果用户当前（时间控制）可以参加的活动只有一个，那么帮助客户跳转到该活动内
+      #转跳方式不可以是直接转跳，最好是跳转用户空间后等待几秒然后进入活动模块，
+      #这样会使用户无法使用用户空间功能（使用序列号参加活动等功能）
     end
   end
 end
